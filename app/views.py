0b093526c7570462209de1f52e84ae00726cd422
Daniel Vilar <danielvilar2@gmail.com>
2017-02-19 17:50:00 +0100

    Añadido al menu lateral el resto de pestañas. Nota: Aun no hay ficheros html para esas pestañas.
    Añadido en views.py las rutas a esas pestañas.

--- app/views.py
+++ app/views.py
@@ -8,3 +8,39 @@ def index():
     resp = make_response(render_template("index.html", title="Home", user=user))
     resp.cache_control.no_cache = True
     return resp
+
+@app.route("/tiempo.html")
+def tiempo():
+    resp = make_response(render_template("tiempo.html"))
+    resp.cache_control.no_cache = True
+    return resp
+
+@app.route("/posicion.html")
+def posicion():
+    resp = make_response(render_template("posicion.html"))
+    resp.cache_control.no_cache = True
+    return resp
+
+@app.route("/sensores.html")
+def sensores():
+    resp = make_response(render_template("sensores.html"))
+    resp.cache_control.no_cache = True
+    return resp
+
+@app.route("/tablas.html")
+def tablas():
+    resp = make_response(render_template("tables.html"))
+    resp.cache_control.no_cache = True
+    return resp
+
+@app.route("/sesiones.html")
+def sesiones():
+    resp = make_response(render_template("sesiones.html"))
+    resp.cache_control.no_cache = True
+    return resp
+
+@app.route("/importar-exportar.html")
+def importar_exportar():
+    resp = make_response(render_template("importar-exportar.html"))
+    resp.cache_control.no_cache = True
+    return resp
