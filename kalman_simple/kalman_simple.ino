a1af984048c5e58e3f95a5d585938bed85afc7a0
ManuLG <idc.manuel@gmail.com>
2017-02-22 11:02:14 +0100

    Kalman simple pulido y comentado

--- kalman_simple/kalman_simple.ino
+++ kalman_simple/kalman_simple.ino
@@ -4,24 +4,23 @@
 #define echoPin 5
 #define maxDistance 500
 
-int distancia = 0;
-
-NewPing sonar(trigPin, echoPin, maxDistance);
-
+// Matrices de Kalman
 float P = 1.0;
 float P_ant = 1.0;
 float P_estimada = 1.0;
 float Q = 1.0;
-float R = 1.0;
+float R = 1.0; // Cuanto mayor es menos confía en los sensores
 float K = 1.0;
 float H = 1.0;
 float F = 1.0;
 
+// Vectores de Kalman
 float x = 1.0;
 float x_ant = 1.0;
 float x_estimada = 1.0;
-
 float z = 0.0;
+
+NewPing sonar(trigPin, echoPin, maxDistance);
 
 void setup() {
   Serial.begin(9600);
@@ -30,15 +29,17 @@ void setup() {
 void loop() {
   z = sonar.ping_cm();
 
-  x = F * x_ant;
+  x = F * x_ant; // Hacemos una predicción sobre el estado
   P = P_ant;
-  
-  K = P / (P + R);
-  x_estimada = x + K * ( z - x);
+
+  K = P / (P + R); // Calculamos la "Ganancia de Kalman"
+  x_estimada = x + K * ( z - x); // Corregimos nuestra predicción en base a los sensores
   P_estimada = (1 - K) * P;
 
   // Visualizar en el Serial Plotter
-  Serial.println(x_extimada);
+  Serial.print(x_estimada); // Azul
+  Serial.print(",");
+  Serial.println(z); // Amarillo
 
   x_ant = x_estimada;
   P_ant = P_estimada;
