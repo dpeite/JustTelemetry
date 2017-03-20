653159f95ae943204d0d842577ec036ba9a873ec
Daniel Vilar <danielvilar2@gmail.com>
2017-03-20 20:44:11 +0100

    Arreglado, ya que se quedaba pillado en las coordenadas 0,0

--- realtime.py
+++ realtime.py
@@ -8,6 +8,7 @@ lon = []
 plt.ion()
 cnt=0
 imprimir = 0
+first = True
 
 serialArduino = serial.Serial('/dev/ttyACM0', 38400)
 
@@ -27,6 +28,11 @@ for i in range(0,5000):
 while True:
     while (serialArduino.inWaiting()==0):
         pass
+    if first:
+    	import time 
+	time.sleep(8)
+	first = False
+
     valueRead = serialArduino.readline()
 
     print valueRead #Para poder copiar lo que sale por terminal a un fichero
@@ -34,21 +40,17 @@ while True:
     asf = valueRead.split("[")
     try:
         coord = asf[1].split(",")
-
+	
         #check if valid value can be casted
         try:
             lat1 = float(coord[0])
             lon1 = float(coord[1]) 
 
-            if lat1 == 0.00000000:
-                imprimir == 1
-            else:
-                if imprimir == 1:
-                    lat.append(lat1)
-                    lon.append(lon1)
-                    lat.pop(0)
-                    lon.pop(0)
-                    drawnow(plotValues)
+            lat.append(lat1)
+            lon.append(lon1)
+            lat.pop(0)
+            lon.pop(0)
+            drawnow(plotValues)
         except ValueError as exc:
             print "Invalid! cannot cast"
             print exc
