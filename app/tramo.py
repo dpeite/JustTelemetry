229896337cadc667829164354579e2a27f0e5807
ManuLG <idc.manuel@gmail.com>
2017-03-28 22:23:25 +0200

    Solucionada la selección inversa de puntos en la trazada #62

--- app/tramo.py
+++ app/tramo.py
@@ -19,5 +19,26 @@ def cortar(lat1, lon1, lat2, lon2, ID, sensor):
                 del info[key][elements]
                 info[key][elements] = datos
                 
+
+    if len(datos) == 0:
+        with open("app/static/data/sesiones/"+ID+"/"+sensor+".json") as data_file:
+            info = json.load(data_file)
+
+        for key in info.keys():
+            for elements in info[key]:
+                if "data" in elements:
+                    datos = []
+                    dentro = False
+                    for index, element in enumerate(info[key][elements]):
+                        print element[2]
+                        if (element[2] == lat2 and element[3] == lon2) or dentro:
+                            print "Dentro"
+                            dentro = True
+                            datos.append(element)
+                        if (element[2] == lat1 and element[3] == lon1):
+                            break;
+
+                    del info[key][elements]
+                    info[key][elements] = datos
+
     return info
-    
