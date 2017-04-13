38561d441066320473a50c611133bc6b15f87417
Daniel Vilar <danielvilar2@gmail.com>
2017-04-13 17:34:51 +0200

    Refactorizar codigo mostrar datos en leyenda #50

--- app/static/js/data/plot.js
+++ app/static/js/data/plot.js
@@ -283,24 +283,29 @@ function vel_ruedas(coord1, coord2, sensor, sensores){
                 break;
               }
             }
-
+	      // j = 1;
             // Now Interpolate
             var y,
             p1 = series.data[j - 1],
             p2 = series.data[j];
-
-            if (p1 == null) {
-              y = p2[1];
-            } else if (p2 == null) {
-              y = p1[1];
-            } else {0
-              y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
-            }
-            $("#"+ids+"-lat").text("Lat: "+ (p1[2] + (p2[2] - p1[2]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
-            $("#"+ids+"-lat").parent().css('visibility', 'visible');
-            $("#"+ids+"-lon").text("Lon: "+(p1[3] + (p2[3] - p1[3]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
-            $("#"+ids+"-lon").parent().css('visibility', 'visible');
-            $("#"+ids+"-"+series.label).text(series.label+" = "+y.toFixed(2))
+	      try {
+		  interpolate(y, p1, p2, ids, pos, series)
+	      }
+	      catch (err){
+		  console.log(err)
+	      }
+            // if (p1 == null) {
+            //   y = p2[1];
+            // } else if (p2 == null) {
+            //   y = p1[1];
+            // } else {0
+            //   y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+            // }
+            // $("#"+ids+"-lat").text("Lat: "+ (p1[2] + (p2[2] - p1[2]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
+            // $("#"+ids+"-lat").parent().css('visibility', 'visible');
+            // $("#"+ids+"-lon").text("Lon: "+(p1[3] + (p2[3] - p1[3]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
+            // $("#"+ids+"-lon").parent().css('visibility', 'visible');
+            // $("#"+ids+"-"+series.label).text(series.label+" = "+y.toFixed(2))
           }
         }
 
--- app/static/js/data/sensores.js
+++ app/static/js/data/sensores.js
@@ -51,3 +51,21 @@ function RefreshSomeEventListener() {
 	console.log($(this).parent().parent().parent())
 });
 }
+
+function interpolate(y, p1, p2, ids, pos, series){
+    if (p1 == null) {
+	console.log(p1)
+	console.log(p2)
+        y = p2[1];
+    } else if (p2 == null) {
+        y = p1[1];
+    } else {
+            y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+           }
+    // $("#"+ids+"-lat").text("Lat: "+ (p1[3] + (p2[3] - p1[3]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
+    // $("#"+ids+"-lat").parent().css('visibility', 'visible');
+    // $("#"+ids+"-lon").text("Lon: "+(p1[4] + (p2[4] - p1[4]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
+    // $("#"+ids+"-lon").parent().css('visibility', 'visible');
+    $("#"+ids+"-"+series.label).text(series.label+" = "+y.toFixed(2))
+    
+}
