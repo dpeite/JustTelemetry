0072f079bb3f5c07f8766a255965d9050974c807
ManuLG <idc.manuel@gmail.com>
2017-04-18 18:02:55 +0200

    Elminado print de debug #108

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,10 +1,6 @@
 function carga() {
 
   $.get("info_sesiones", null, function(data, status, xhr){
-  		console.log("Esto es un mundo")
-
-  		console.log(data)
-
   		var plot = $.plot("#morris-bar-chart", [data]);
   })
   .fail(function(response) {
