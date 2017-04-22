7f8f4f9bf89bd2616b9cc08db09687c57c216bee
Daniel Vilar <danielvilar2@gmail.com>
2017-04-22 16:10:50 +0200

    Arreglado fallo en script generacion valores falsos

--- app/static/data/sesiones/7/fake.py
+++ app/static/data/sesiones/7/fake.py
@@ -4,18 +4,29 @@ import random
 with open('ruedas2.json') as json_data:
     d = json.load(json_data)
     # print(d)
-
+first = True
+array_dist = []
 for key in d.keys():
     a = random.randint(10,50)
+    dist = 0
     for elements in d[key]:
         if "data" in elements:
-            for element in d[key][elements]:
+            for idx, element in enumerate(d[key][elements]):
                 b = random.randint(-5,5)
                 a += b
                 element.append(a)
                 element[3], element[1] = element[1], element[3]
                 element[2], element[0] = element[0], element[2]
-                print element
+                if first:
+                    c = random.randint(0,5)
+                    dist += c
+                    element.append(dist)
+                    array_dist.append(dist)
+                else:
+                    element.append(array_dist[idx])
+                # print element
+            first = False
+
     
 with open('ruedas.json', 'w') as outfile:
         json.dump(d, outfile)
