ea4ca4c595475e9de73aa75c5a9b00ec949e8897
Daniel Vilar <danielvilar2@gmail.com>
2017-04-27 16:09:16 +0200

    Cambiadas coordenadas por tiempo y distancia #120

--- app/static/js/data/posicion.js
+++ app/static/js/data/posicion.js
@@ -39,9 +39,12 @@ function interpolate(y, p1, p2, ids, pos, series){
   } else {0
     y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
   }
-  $("#"+ids+"-lat").text("Lat: "+ (p1[2] + (p2[2] - p1[2]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
-  $("#"+ids+"-lat").parent().css('visibility', 'visible');
-  $("#"+ids+"-lon").text("Lon: "+(p1[3] + (p2[3] - p1[3]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
+  // $("#"+ids+"-lat").text("Lat: "+ (p1[2] + (p2[2] - p1[2]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
+    $("#"+ids+"-lat").text("Tiempo: "+ (pos.x/ 1000).toFixed(2)+"s")
+    $("#"+ids+"-lat").parent().css('visibility', 'visible');
+    // $("#"+ids+"-lon").text("Lon: "+(p1[3] + (p2[3] - p1[3]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2))
+    $("#"+ids+"-lon").text("Distancia: "+(p1[4] + (p2[4] - p1[4]) * (pos.x - p1[0]) / (p2[0] - p1[0])).toFixed(2)+"m")
+
   $("#"+ids+"-lon").parent().css('visibility', 'visible');
   $("#"+ids+"-"+series.label).text(series.label+" = "+y.toFixed(2))
 }
